d9105723d321428b5bd17c37828f851b346d1c35
Sowmi3003 <sowmithasudha@gmail.com>
2023-10-28 01:57:28 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,83 @@
+import { trigger, transition, style, animate } from '@angular/animations';
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faCode,
+  faLaptopCode,
+  faDatabase,
+  faServer,
+  faGraduationCap,
+  faBriefcase,
+  faInfinity,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [FontAwesomeModule, NgIf, NgClass],
+  animations: [
+    trigger('educationInsertRemoveTrigger', [
+      transition(':enter', [
+        animate(
+          '300ms ease-out',
+          style({ opacity: 0, transform: 'translateX(-100%)' })
+        ),
+        animate(
+          '300ms ease-out',
+          style({ opacity: 1, transform: 'translateX(0%)' })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '300ms ease-out',
+          style({ opacity: 0, transform: 'translateX(100%)' })
+        ),
+      ]),
+    ]),
+    trigger('workInsertRemoveTrigger', [
+      transition(':enter', [
+        animate(
+          '300ms ease-out',
+          style({ opacity: 0, transform: 'translateX(-100%)' })
+        ),
+        animate(
+          '300ms ease-out',
+          style({ opacity: 1, transform: 'translateX(0%)' })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '300ms ease-out',
+          style({ opacity: 0, transform: 'translateX(-100%)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'portfolio';
+  faLaptopCode = faLaptopCode;
+  faCode = faCode;
+  faDB = faDatabase;
+  faServer = faServer;
+  faCap = faGraduationCap;
+  faWork = faBriefcase;
+  faDevops = faInfinity;
+
+  isEducation = true;
+  isWork = false;
+
+  onClickToggle(key: any) {
+    if (key === 'qualification') {
+      this.isWork = false;
+      this.isEducation = true;
+    }
+    if (key === 'work') {
+      this.isWork = true;
+      this.isEducation = false;
+    }
+  }
 }
